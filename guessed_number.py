432108af436d1e39cd0e1067c46e145fd3659444
Andre Luiz Emidio Junior <andre.junior@engdb.com.br>
2022-02-03 04:46:01 -0300

    Aceitar apenas numerico

--- guessed_number.py
+++ guessed_number.py
@@ -12,33 +12,37 @@ def guess_number ():
     guessed_number =  False
    
     while guessed_number is False:
-        user_guesses = int(input(" Put your number to guess > "))
+        user_guesses =   input(" Put your number to guess > ")
 
-        if user_guesses > 100:
-            print (" Please, guess a number between 1 - 100 \n")
-            print (f" {user_guesses} is greater than 100 \n ")
+        if user_guesses.isdigit() is True:
 
-        elif user_guesses <= guess_number:
-            print ("Your guess is too high, try again \n")
+            user_guesses = int(user_guesses)
 
-        elif user_guesses >= guess_number:
-            print (" Your guess is too low, try again \n")
+            if user_guesses > 100:
+                print (" Please, guess a number between 1 - 100 \n")
+                print (f" {user_guesses} is greater than 100 \n ")
 
-        if user_guesses == guess_number:
-            print ("Congratulations! You guessed the number!!")
+            elif user_guesses <= guess_number:
+                print ("Your guess is too high, try again \n")
+
+            elif user_guesses >= guess_number:
+                print (" Your guess is too low, try again \n")
+
+            if user_guesses == guess_number:
+                print ("Congratulations! You guessed the number!!")
 
 
-            print (" Would you like to play again? \n")
-            question = input(" Press Y (yes) or N (not)  >> ")
+                print (" Would you like to play again? \n")
+                question = input(" Press Y (yes) or N (not)  >> ")
 
-            if question.lower() == 'y':
-                guess_number = randint(1,100)
-                print(guess_number)
-                continue
-            
-            else:
-                matched = True
-                break
+                if question.lower() == 'y':
+                    guess_number = randint(1,100)
+                    print(guess_number)
+                    continue
+                
+                else:
+                    matched = True
+                    break
 
 guess_number ()
    
